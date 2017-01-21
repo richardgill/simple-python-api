95053e801d8efdc7cfe5d5f014535955933c021d
Richard Gill <richard@rgill.co.uk>
2017-01-21 17:49:18 +0000

    Fixed environment getting

--- http_server.py
+++ http_server.py
@@ -10,8 +10,8 @@ def hello():
   response.content_type = 'application/json'
   return json.dumps({'hello': "world"})
 
-if 'PYTHON_ENV' in os.environ:
+if os.environ.get('PYTHON_ENV') == 'PRODUCTION':
   print('Running in prod :serious_face:')
-  run(host='0.0.0.0', port=os.environ.PORT)
+  run(host='0.0.0.0', port=os.environ.get('PORT'))
 else:
   run(host='localhost', port=8080, debug=True)
