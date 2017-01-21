25d14d284bc2a294e008614be78a51327ba3a8be
Richard Gill <richard@rgill.co.uk>
2017-01-21 17:56:54 +0000

    Fixed post up

--- http_server.py
+++ http_server.py
@@ -1,12 +1,19 @@
-from bottle import request, response, route, run
+from bottle import get, post, request, response, run
 import json
 import os
 
-@route('/milanFunction')
-def hello():
+@get('/milanFunction')
+def milanFunction():
   input = request.query.getall('input[]')
   print(input)
   print(type(input))
+  response.content_type = 'application/json'
+  return json.dumps({'hello': "world"})
+
+@post('/milanFunction')
+def milanFunctionPost():
+  print(request.json)
+  print(type(request.json['input']))
   response.content_type = 'application/json'
   return json.dumps({'hello': "world"})
 
@@ -15,3 +22,5 @@ if os.environ.get('PYTHON_ENV') == 'PRODUCTION':
   run(host='0.0.0.0', port=os.environ.get('PORT'))
 else:
   run(host='localhost', port=8080, debug=True)
+
+
